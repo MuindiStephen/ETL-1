6be6ae724ca36ddb67ec9951fe41215a3a7094cf
muindi.stephen <muindi.stephen@eclectics.io>
2025-04-19 01:45:52 +0300

    ETL

---
+++ weather_etl.py
@@ -0,0 +1,53 @@
+"""""
+Use python to create a simple ETL data pipeline to extract, transform and load weather 
+from a REST API to save as a CSV file
+"""
+#import os
+import requests
+import pandas as pd
+from datetime import datetime
+#from dotenv import load_dotenv
+
+
+##load sensitive data from .env
+#load_dotenv()
+#api_key = os.getenv("API_KEY")
+API_KEY = '92ac1561e5d0eccf9f54619896ddc504'
+CITY = 'Nairobi'
+
+
+
+# Define a base url for Open Weather 
+URL = f'http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric'
+
+# Extract data
+def extract():
+    response = requests.get(URL)
+    data = response.json()
+    return data
+
+## Transform data
+def transform(data):
+    transformed_data = {
+        'city': data['name'],
+        'temperature': data['main']['temp'],
+        'humidity': data['main']['humidity'],
+        'weather': data['weather'][0]['description'],
+        'timestamp': datetime.now()
+    }
+    return transformed_data
+
+## Load data
+def load(data):
+    df = pd.DataFrame([data])
+    df.to_csv('weather_data.csv', mode='a', header=not pd.io.common.file_exists('weather_data.csv'), index=False)
+    print("Data Loaded successfuly")
+
+def run_etl():
+    raw_data = extract()
+    processed_data = transform(raw_data)
+    load(processed_data)
+
+if __name__ == '__main__':
+    run_etl()
+
