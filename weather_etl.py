dfe26fdb509268d33b627ed47da899de359115bc
muindi.stephen <muindi.stephen@eclectics.io>
2025-04-19 18:13:47 +0300

    Postgres Integration

--- weather_etl.py
+++ weather_etl.py
@@ -18,15 +18,18 @@ API_KEY = '92ac1561e5d0eccf9f54619896ddc504'
 CITY = 'Nairobi'
 
 # PostgreSql db credentials
-DB_USER = ''
+DB_USER = 'postgres'
 DB_PASSWORD = 'admin1234'
 DB_HOST = 'localhost'  # or your database host
 DB_PORT = '5432'       # default PostgreSQL port
 DB_NAME = 'weather_db'
 
 
-# Define a base url for Open Weather 
+# Define a base url for Open Weather  - fstring
 URL = f'http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric'
+
+
+
 
 # Extract data
 def extract():
@@ -48,8 +51,18 @@ def transform(data):
 ## Load data
 def load(data):
     df = pd.DataFrame([data])
-    df.to_csv('weather_data.csv', mode='a', header=not pd.io.common.file_exists('weather_data.csv'), index=False)
-    print("Data Loaded successfuly")
+   # df.to_csv('weather_data.csv', mode='a', header=not pd.io.common.file_exists('weather_data.csv'), index=False)
+    #print("Data Loaded successfuly")
+    # Establish a connection using SQLAlchemy Engine
+    engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+    # Load data into PostgreSQL
+    table_name = 'weather_data'
+    df.to_sql(table_name, engine, if_exists='append', index=False)
+    print('Data loaded successfully into PostgreSQL.')
+
+
+    # Close the engine connection 
+    engine.dispose()
 
 def run_etl():
     raw_data = extract()
